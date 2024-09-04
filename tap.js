4b570374cf3d10d0043f5e134d2291213abad46c
kbnzhpp <pakseevf@mail.ru>
2024-09-05 00:40:35 +0700

    alexey

--- tap.js
+++ tap.js
@@ -1,9 +1,18 @@
+var max_img = 6
+function Random() {
+    return Math.floor(Math.random() * (max_img - 1) + 1)
+}
 document.addEventListener('DOMContentLoaded', () => {
-    const button = document.querySelector('#button1');
-
+    const button = document.querySelector('#button1')
+    const showleha = document.querySelector('.showleha')
+    const photo = document.querySelector('.photo')
     if (button) {
-        button.addEventListener('click', () => {
-            alert("АХУЕЛА МРАЗОТА");
+        button.addEventListener('click', (event) => {
+            var final_img = "imgs/" + Random() + '.jpg'
+            photo.setAttribute('src', final_img)
+            showleha.setAttribute('style', 'display: block')
+            event.preventDefault()
         });
-    }
-});
+    };
+})
+
