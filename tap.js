878bdb5edbbbb8f200e9a1dfebfc19990cd67edb
kbnzhpp <pakseevf@mail.ru>
2024-09-05 02:38:28 +0700

    alexey3

--- tap.js
+++ tap.js
@@ -1,18 +1,23 @@
 var max_img = 7
+
 function Random() {
     return Math.floor(Math.random() * (max_img - 1) + 1)
 }
+
 document.addEventListener('DOMContentLoaded', () => {
     const button = document.querySelector('#button1')
-    const showleha = document.querySelector('.showleha')
     const photo = document.querySelector('.photo')
+    const showleha = document.querySelector('.showleha')
     if (button) {
         button.addEventListener('click', (event) => {
-            var final_img = "imgs/" + Random() + '.jpg'
-            photo.setAttribute('src', final_img)
-            showleha.setAttribute('style', 'display: block')
-            event.preventDefault()
+            event.preventDefault() // prevent update of page
+            showleha.style.opacity = 0
+            // do a lil delay to apply animation
+            setTimeout(() => {
+                var final_img = "imgs/" + Random() + '.jpg'
+                photo.setAttribute('src', final_img)
+                showleha.style.opacity = 1 // turn off animation again
+            }, 1000); // 1000 ms delay
         });
-    };
+    }
 })
-
