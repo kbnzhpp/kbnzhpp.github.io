3b81976d6a73a80e66a02715539763f2bbad4b06
kbnzhpp <pakseevf@mail.ru>
2024-10-22 02:21:11 +0700

    ultra legend v1.1

--- tap.js
+++ tap.js
@@ -14,23 +14,48 @@ function Random() {
 
 // Open collection
 function open_bar() {
+    const collection = document.getElementById("collection-images");
     document.getElementById("collection-images").style.width = "95%";
     document.getElementById("arrow-right").style.display = "none";
     document.getElementById("arrow-left").style.display = "block";
+    collection.style.borderRadius = "10px";
+    collection.style.border = "2px solid #272727";
+    collection.style.backgroundColor = "#1b1b1b";
 }
 
 // Close collection
 function close_bar() {
+    const collection = document.getElementById("collection-images");
     document.getElementById("collection-images").style.width = "0";
     document.getElementById("arrow-right").style.display = "block";
     document.getElementById("arrow-left").style.display = "none";
+    collection.style.borderRadius = "";
+    collection.style.border = "";
+    collection.style.backgroundColor = "";
 }
 
 //Return to the back after open ultra legend
 function back() {
-    
-}
+    const main = document.querySelectorAll('.main *:not(.ultra-legend-opened):not(.ultra-legend-opened .ultra-legend)')
+    const back_button = document.querySelector(".back-button");
 
+    const ultra_div = document.querySelector(".ultra-legend-opened");
+    ultra_div.classList.add('hidden-opacity');
+    setTimeout(() => {
+        
+        main.forEach(elem => {
+            elem.classList.remove('hidden-display');
+        })
+        back_button.style.opacity = '0'
+        setTimeout(() => {
+            main.forEach(elem => {
+                elem.classList.remove('hidden-opacity');
+            })
+            ultra_div.classList.add('hidden-display');
+            back_button.style.display = 'none'
+        }, 600);
+    }, 600);
+}
 // Random fill and change of photos
 document.addEventListener('DOMContentLoaded', () => {
     const button1 = document.querySelector('#button1')
@@ -62,12 +87,16 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 // Unlock of ultra-legend card
-
 document.addEventListener('DOMContentLoaded', () => {
     const button1 = document.querySelector('#button1')
     const button_ultra = document.querySelector('#button-ultra')
-    const photo = document.querySelector("#ultra-photo")
+    const photo = document.querySelector(".ultra-legend")
+    const ultra_div = document.querySelector('.ultra-legend-opened')
+    const main = document.querySelectorAll('.main *:not(.ultra-legend-opened):not(.ultra-legend-opened *)')
+
+    console.log(main)
     count = 0
+
     button1.addEventListener('click', () => {
         count += 50
         if (count >= 100) {
@@ -82,13 +111,27 @@ document.addEventListener('DOMContentLoaded', () => {
                 button_ultra.style.animation = "none"
             }, 500)
         } else {
-            
-            document.body.classList.add('global-transition');
+            button_ultra.setAttribute("disabled", "")
+            main.forEach(element => {
+                element.classList.add("hidden-opacity");
+            });
             setTimeout(() => {
-                document.body.classList.add('global-display');
-                
+                main.forEach(element => {
+                    element.classList.add("hidden-display");
+                });
+                photo.classList.remove('hidden-display')
                 setTimeout(() => {
-                    photo.style.opacity = "1";
+                    photo.classList.remove('hidden-opacity');  
+
+                    // Create back button
+                    const button = document.createElement('button');
+                    button.className = 'back-button';
+                    button.innerText = 'Вернуться назад';
+                    button.onclick = () => back();
+                    ultra_div.appendChild(button);
+                    setTimeout(() => {
+                        button.style.opacity = "1";
+                    }, 1000);
                 }, 600);
             }, 600);
         }
